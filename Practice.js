ef0ae5ce07f9e045afa7cf8e98f018975abc2956
Shrey Talreja <shreytalreja25@gmail.com>
2023-06-20 23:33:47 +0530

    Commit

---
+++ Practice.js
@@ -0,0 +1,9 @@
+const numbers = [1, 2, 3, 4];
+const doubled = numbers.map((num) => num * 2);
+console.log(typeof(doubled));
+
+function Hello (){
+    console.log("Hello")
+}
+
+console.log(typeof(Hello))
